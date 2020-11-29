5df8844360900ffe5195fff55f438c822c22742f
baryo <or.bar.yosef@sap.com>
2020-11-29 18:44:12 +0200

    extending application model

--- entities/common/config.ts
+++ entities/common/config.ts
@@ -1,9 +1,19 @@
 import {JSONSchema7} from "json-schema";
 
 export type ConfigOverrideScope = 'application' | 'action' | 'journey-action' | 'event';
+export type ResourcePath = string;
 
 export type WithConfigSchema = { configSchema?: JSONSchema7 & { type: 'object'; properties: { [propName: string]: JSONSchema7 & { scope: ConfigOverrideScope[] } } }; };
 export type WithConfigValues = { configValues?: Record<string, any>; };
+export type WithPollingConfig = {
+    pollingConfig?: Record<ResourcePath, {
+        pageFieldName: string;
+        recordsPath?: string;
+    }>;
+};
 export type WithResourcePath = {
-    resourcePath: string; // how we find the model/schema of the Connector
+    resourcePath: ResourcePath; // how we find the model/schema of the Connector
+};
+export type WithTestResourcePath = {
+    testResourcePath?: ResourcePath; // how we find the model/schema of the Connector
 };
