0d21ba4ec8e5fd93c424cfc8ddc79970e25b35f9
baryo <or.bar.yosef@sap.com>
2020-12-19 15:33:54 +0200

    journey api review

--- entities/Journey/index.ts
+++ entities/Journey/index.ts
@@ -1,6 +1,8 @@
-import {Entity, Id} from "../common";
+import {Entity, Id, Payload} from "../common";
 import {JourneyStep, JourneyStepId} from "./JourneySteps";
 import {JourneyCondition} from "./JourneyCondition";
+import {BusinessUnitId} from "../BusinessUnit";
+import {ViewId} from "../View";
 
 export type JourneyId = Id;
 
@@ -8,4 +10,25 @@ export interface Journey extends Entity<JourneyId> {
     steps: JourneyStep[];
     trigger: JourneyCondition;
     then: JourneyStepId[];
+}
+
+type JourneyStatus = {
+    status: 'Ready' | 'In Provisioning' | 'Error';
+    errorDetails: string;
+};
+
+type WithJourneyState = {
+    state: JourneyStatus
+};
+
+interface JourneyEndpoints {
+    '/businessunits/$bUnitId/views/$viewId/journeys/$journeyId': {
+        GET: (bUnitId: BusinessUnitId, viewId: ViewId, journeyId: JourneyId) => Promise<Journey & WithJourneyState>;
+        PUT: (bUnitId: BusinessUnitId, viewId: ViewId, journeyId: JourneyId, payload: Payload<Journey>) => Promise<Journey & WithJourneyState>;
+        DELETE: (bUnitId: BusinessUnitId, viewId: ViewId, journeyId: JourneyId) => Promise<void>;
+    };
+
+    '/businessunits/$bUnitId/views/$viewId/journeys/$journeyId/status': {
+        GET: (bUnitId: BusinessUnitId, viewId: ViewId, journeyId: JourneyId) => Promise<JourneyStatus>;
+    };
 }
